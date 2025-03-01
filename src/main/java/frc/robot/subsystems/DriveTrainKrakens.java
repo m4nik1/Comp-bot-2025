fea150cf6af079102a36162e9206abd2cf3818c1
m4nik1 <maniksoomro@hotmail.com>
2025-02-28 22:27:24 -0500

    Start of drive code

--- src/main/java/frc/robot/subsystems/DriveTrainKrakens.java
+++ src/main/java/frc/robot/subsystems/DriveTrainKrakens.java
@@ -4,10 +4,27 @@
 
 package frc.robot.subsystems;
 
+import java.lang.reflect.Field;
+import java.util.Optional;
+
+import org.photonvision.EstimatedRobotPose;
+
 import com.ctre.phoenix6.hardware.Pigeon2;
 import com.ctre.phoenix6.signals.InvertedValue;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator3d;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
 
 public class DriveTrainKrakens extends SubsystemBase {
   /** Creates a new TestDrive. */
@@ -15,17 +32,85 @@ public class DriveTrainKrakens extends SubsystemBase {
   ElmCityKrakenModule[] elmCityModules;
 
   Pigeon2 gyro;
+  Pose2d robotPose;
 
+  SwerveDrivePoseEstimator odom;
+  Field2d field;
 
 
   public DriveTrainKrakens() {
     elmCityModules = new ElmCityKrakenModule[] {
-      new ElmCityKrakenModule(0, 1, 2, InvertedValue.CounterClockwise_Positive, InvertedValue.Clockwise_Positive),
-      new ElmCityKrakenModule(1, 15, 14,  InvertedValue.Clockwise_Positive, InvertedValue.Clockwise_Positive),
-      new ElmCityKrakenModule(2, 1, 2, InvertedValue.CounterClockwise_Positive, InvertedValue.Clockwise_Positive),
-      new ElmCityKrakenModule(3, 4, 3, InvertedValue.Clockwise_Positive, InvertedValue.Clockwise_Positive)
+      new ElmCityKrakenModule(0, 1, 2, 0, Constants.angleOffsetMod0, InvertedValue.CounterClockwise_Positive, InvertedValue.Clockwise_Positive),
+      // new ElmCityKrakenModule(1, 15, 14, 25, Constants.angleOffsetMod1, InvertedValue.Clockwise_Positive, InvertedValue.Clockwise_Positive),
+      // new ElmCityKrakenModule(2, 1, 2, 26, Constants.angleOffsetMod2, InvertedValue.CounterClockwise_Positive, InvertedValue.Clockwise_Positive),
+      // new ElmCityKrakenModule(3, 4, 3, 27, Constants.angleOffsetMod3, InvertedValue.Clockwise_Positive, InvertedValue.Clockwise_Positive)
     };
-    gyro = new Pigeon2(21);
+
+    field = new Field2d();
+    gyro = new Pigeon2(Constants.pigeonID);
+
+    // odom = new SwerveDrivePoseEstimator(Constants.swerveKinematics, getYaw(), getPositions(), new Pose2d());
+    
+    resetGyro();
+
+    // new Thread(() -> {
+    //   try {
+    //     Thread.sleep(1000);
+    //     odom.resetPosition(new Rotation2d(), getPositions(), new Pose2d());
+    //     } catch(Exception e) {}
+    // }).start();
+  }
+
+  public Rotation2d getYaw() {
+    return Rotation2d.fromDegrees(gyro.getYaw().getValueAsDouble());
+  }
+
+  public void setDriveVelocity() {
+    for(ElmCityKrakenModule m : elmCityModules) {
+      m.runVelocity(1);
+    }
+  }
+
+  public SwerveModulePosition[] getPositions() {
+    SwerveModulePosition[] positions = new SwerveModulePosition[4];
+
+    for(ElmCityKrakenModule k : elmCityModules) {
+      positions[k.modNum] = elmCityModules[k.modNum].getPosition();
+    }
+
+    return positions;
+  }
+
+
+  public void resetGyro() {
+    gyro.getConfigurator().setYaw(0.0);
+
+    gyro.reset();
+  }
+
+  public Pose2d getRobotPose2d() {
+    Pose2d robotPose = odom.update(getYaw(), getPositions());
+
+    return robotPose;
+  }
+
+  public void resetPose(Pose2d pose) {
+    odom.resetPose(pose);
+  }
+
+  public void updatePose() {
+    odom.update(getYaw(), getPositions());
+
+    Optional<EstimatedRobotPose> photonPose = RobotContainer.photonCamera.getEstimatedGlobalPose();
+
+    // Adds Vision measurement for odometry
+    odom.setVisionMeasurementStdDevs(VecBuilder.fill(.7, .7, 999999)); 
+    odom.addVisionMeasurement(photonPose.get().estimatedPose.toPose2d(), photonPose.get().timestampSeconds);
+  }
+
+  public void setAngle(double deg) {
+    elmCityModules[0].goToAngle(deg);
+    // elmCityModules[4].goToAngle(deg);
   }
 
   @Override
---
+++ src/main/java/frc/robot/subsystems/ElmCityModule.java
@@ -0,0 +1,208 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.PositionDutyCycle;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.wpilibj.AnalogEncoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class ElmCityModule extends SubsystemBase {
+  /** Creates a new ElmCityKrakenModule. */
+
+  TalonFX driveMotor;
+  TalonFX angleMotor;
+
+  AnalogEncoder nacCoder;
+
+
+  PositionDutyCycle anglePosition = new PositionDutyCycle(0);
+  DutyCycleOut driveOpenLoop;
+  VelocityVoltage driveVelocity;
+  SimpleMotorFeedforward driveKfCalc = new SimpleMotorFeedforward(Constants.driveKs, Constants.driveKv, Constants.drivekA);
+
+  public int modNum;
+  public double velocitySet;
+
+  Rotation2d lastAngle;
+  Rotation2d offset;
+  InvertedValue driveInverted;
+
+  public ElmCityModule(int moduleNumber, int driveID, int angleID, int nacID, Rotation2d givenOffset,
+      InvertedValue driveInvert,
+      InvertedValue angleInvert) {
+    this.modNum = moduleNumber;
+    this.offset = givenOffset;
+    this.driveInverted = driveInvert;
+
+    driveMotor = new TalonFX(driveID);
+    angleMotor = new TalonFX(angleID);
+    nacCoder = new AnalogEncoder(nacID);
+
+    velocitySet = 0;
+
+    // 0 is default position
+    anglePosition.Slot = 0;
+
+    driveOpenLoop = new DutyCycleOut(0);
+    driveVelocity = new VelocityVoltage(0);
+    driveVelocity.Slot = 0;
+
+    configDriveMotor(driveInvert);
+    configAngleMotor();
+
+    driveMotor.setPosition(0.0);
+    lastAngle = Rotation2d.fromDegrees(0);
+    // resetToAbsolute();
+    angleMotor.setPosition(0);
+  }
+
+  public void configDriveMotor(InvertedValue drive) {
+    TalonFXConfiguration driveConfig = new TalonFXConfiguration();
+    driveMotor.getConfigurator().apply(new TalonFXConfiguration());
+
+    driveConfig.MotorOutput.Inverted = drive;
+    driveConfig.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
+    driveConfig.Feedback.SensorToMechanismRatio = Constants.driveRatio; ///((Math.PI)*Units.inchesToMeters(4))
+    driveConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+
+    driveConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+    driveConfig.CurrentLimits.StatorCurrentLimit = 60;
+
+    driveConfig.OpenLoopRamps.DutyCycleOpenLoopRampPeriod = 0.1;
+    driveConfig.ClosedLoopRamps.DutyCycleClosedLoopRampPeriod = 0.1;
+    driveConfig.ClosedLoopRamps.VoltageClosedLoopRampPeriod = 0.1;
+
+    driveConfig.Slot0.kP = Constants.driveKp;
+    driveConfig.Slot0.kI = Constants.driveKi;
+    driveConfig.Slot0.kD = Constants.driveKd;
+
+    driveMotor.getConfigurator().apply(driveConfig);
+    driveMotor.getConfigurator().setPosition(0.0);
+
+
+  }
+
+  public void configAngleMotor() {
+    TalonFXConfiguration angleConfig = new TalonFXConfiguration();
+    angleMotor.getConfigurator().apply(new TalonFXConfiguration());
+
+    angleConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+    angleConfig.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
+    angleConfig.Feedback.SensorToMechanismRatio = Constants.angleRatio;
+    angleConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+    angleConfig.ClosedLoopGeneral.ContinuousWrap = true;
+
+    angleConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+    angleConfig.CurrentLimits.StatorCurrentLimit = 60;
+
+    angleConfig.OpenLoopRamps.DutyCycleOpenLoopRampPeriod = 0.1;
+    angleConfig.ClosedLoopRamps.VoltageClosedLoopRampPeriod = 0.1;
+
+    angleConfig.Slot0.kP = Constants.angleP;
+    angleConfig.Slot0.kI = Constants.angleI;
+    angleConfig.Slot0.kD = Constants.angleD;
+
+    angleMotor.getConfigurator().apply(angleConfig);
+  }
+
+  public void resetToAbsolute() {
+    double absoluteValue = getNac() - offset.getRotations();
+    angleMotor.setPosition(absoluteValue);
+  }
+
+  public SwerveModulePosition getPosition() {
+    return new SwerveModulePosition(getDrivePosConversion(), Rotation2d.fromRotations(getAngle()));
+  }
+
+  public double getNac() {
+    return nacCoder.get();
+  }
+
+  public double getDrivePosition() {
+    return driveMotor.getPosition().getValueAsDouble();
+  }
+
+  public double getDriveVel() {
+    return driveMotor.getVelocity().getValueAsDouble();
+  }
+
+  public double getAngle() {
+    return angleMotor.getPosition().getValueAsDouble();
+  }
+
+  public void runVelocity(double vel) {
+    driveVelocity.Velocity = vel / Constants.wheelCircum; // meters per second to rotations per second
+
+    velocitySet = vel;
+
+
+    driveVelocity.Slot = 0;
+
+      // sets the feedforward to simple feedforward calculation with the requested
+    driveVelocity.FeedForward = driveKfCalc.calculate(vel);
+
+      // Calculate using feedforward
+    driveMotor.setControl(driveVelocity);
+  }
+
+  public void goToAngle(double deg) {
+    anglePosition.withSlot(0);
+    anglePosition.Position = Rotation2d.fromDegrees(deg).getRotations();
+
+    angleMotor.setControl(anglePosition);
+  }
+
+  public double getDriveVelocityConversion() {
+    double meters = driveMotor.getVelocity().getValueAsDouble() * Constants.wheelCircum;
+    // SmartDashboard.putString("Units of velocity", driveMotor.getVelocity().getUnits());
+    
+    return meters;
+  }
+
+  public void zeroAngleCoders() {
+    // nacCoder.
+  }
+
+  public double getDrivePosConversion() {
+    double velocity = driveMotor.getPosition().getValueAsDouble() * Constants.wheelCircum;
+    // SmartDashboard.putString("Units of velocity", driveMotor.getVelocity().getUnits());
+    
+    return velocity;
+  }
+
+  public void goToAngle2(double deg) {
+    anglePosition.withSlot(0);
+
+    anglePosition.Position = Rotation2d.fromDegrees(deg).getRotations();
+    angleMotor.setControl(anglePosition);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Module Angle " + modNum, getAngle());
+    SmartDashboard.putNumber("Encoder " + modNum, getNac());
+    // SmartDashboard.putNumber("Swerve Velocity " + modNum, getDriveVelocityConversion());
+    // SmartDashboard.putNumber("Drive Distance " + modNum, getDrivePosConversion());
+    // SmartDashboard.putNumber("Drive Velocity Wanted" + modNum, velocitySet);
+    // SmartDashboard.putNumber("Swerve Motor Voltage " + modNum, driveMotor.getMotorVoltage().getValueAsDouble());
+    // SmartDashboard.putNumber("Swerve Motor Supply " + modNum, driveMotor.getSupplyVoltage().getValueAsDouble());
+    // SmartDashboard.putNumber("Nac Angle " + modNum, getNac());
+    // SmartDashboard.putNumber("Rotation 90", Rotation2d.fromDegrees(90).getRotations());
+  }
+}
