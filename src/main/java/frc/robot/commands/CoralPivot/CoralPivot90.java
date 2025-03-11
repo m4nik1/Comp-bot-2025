a641c037dfa811d488fb8d0afc4961ae2da9170e
m4nik1 <maniksoomro@gmail.com>
2025-03-11 01:41:32 -0400

    Moved coral pivot, added reset position to SwerveDriveOdom

--- src/main/java/frc/robot/commands/CoralPivot/CoralPivot90.java
+++ src/main/java/frc/robot/commands/CoralPivot/CoralPivot90.java
@@ -2,50 +2,38 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.CoralPivot;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.RobotContainer;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class RunElevatorManual extends Command {
-  /** Creates a new RunElevatorVolts. */
-
-  boolean stop = false;
-  public RunElevatorManual() {
+public class CoralPivot90 extends Command {
+  /** Creates a new SetCoralPivot. */
+  public CoralPivot90() {
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(RobotContainer.elevator);
+    // addRequirements(RobotContainer.coralPivot);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-    stop = false;
-  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-
-    // if(RobotContainer.getStartBtn()) {
-      double speed = -RobotContainer.getLeftYOp();
-
-      if(RobotContainer.elevator.getTopLimit()) {
-        stop = true;
-      }
-
-      RobotContainer.elevator.driveElevatorPercent(speed);
-    // }
+    RobotContainer.coralPivot.setPivot(-3);
+    SmartDashboard.putNumber("Pivot setpoint", -3);
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return stop;
+    return false;
   }
 }
--- src/main/java/frc/robot/commands/Elevator_Postions/RunElevatorManual.java
+++ src/main/java/frc/robot/commands/Elevator_Postions/RunElevatorManual.java
@@ -2,36 +2,50 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.Elevator_Postions;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.RobotContainer;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class CoralPivotDown extends Command {
-  /** Creates a new CoralPivotDown. */
-  public CoralPivotDown() {
+public class RunElevatorManual extends Command {
+  /** Creates a new RunElevatorVolts. */
+
+  boolean stop = false;
+  public RunElevatorManual() {
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(RobotContainer.coralPivot);
+    addRequirements(RobotContainer.elevator);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    stop = false;
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    RobotContainer.coralPivot.setPivot(4.2);
+
+    // if(RobotContainer.getStartBtn()) {
+      double speed = -RobotContainer.getLeftYOp();
+
+      if(RobotContainer.elevator.getTopLimit()) {
+        stop = true;
+      }
+
+      RobotContainer.elevator.driveElevatorPercent(speed);
+    // }
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return stop;
   }
 }
