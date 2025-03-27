728f257519a95ef9baab373dfe988120acfb9988
Cuescript <cuescript@Cuescripts-Mac-mini.local>
2025-03-27 16:16:43 -0400

    Fixed Drive function indent

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -142,7 +142,7 @@ public class DriveTrain extends SubsystemBase {
     gyro.reset();
   }
 
-    public void drive(Translation2d translation, double rotation) {
+  public void drive(Translation2d translation, double rotation) {
     SwerveModuleState[] moduleStates;
 
     ChassisSpeeds spds = ChassisSpeeds.fromFieldRelativeSpeeds(translation.getX(), translation.getY(), rotation, getYaw());
